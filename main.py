2c6aa901d629eee60f73c6a26bd068a09e020d09
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@epn.edu.ec>
2024-12-12 15:34:29 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def index():
+    var = 5
+    return render_template("index.html", variable = var)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5111, debug=True)
