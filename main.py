84002d37424e5f87cdd7818da86cd172be684970
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@epn.edu.ec>
2024-12-12 22:09:01 -0500

    archivos html de base

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template("index.html")
 
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+@app.route("/index2")
+def index2():
+    return render_template("index2.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5111, debug=True)
