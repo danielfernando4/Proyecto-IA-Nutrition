2abe08567b69ba4021cf145bcc74a633efc12897
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@epn.edu.ec>
2024-12-12 22:06:11 -0500

    herencia html

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    var = 5
-    return render_template("index.html", variable = var)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5111, debug=True)
