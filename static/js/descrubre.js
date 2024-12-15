f50d7b62d014863857062eff5085963769399963
CESAR HECTOR PANTOJA ARMAS <cesar.pantoja01@epn.edu.ec>
2024-12-15 17:00:00 -0500

    Modal para las recetas

--- static/js/descrubre.js
+++ static/js/descrubre.js
@@ -7,4 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
             panelInfo.classList.toggle("open");
         });
     }
+
+    const abrirReceta = document.getElementById("ver_infoRec");
+    const cerrarReceta = document.getElementById("close_recipeinf");
+    const panelReceta = document.getElementById("recipe_info");
+    const overlay = document.getElementById("modal_overlay"); // Referencia al overlay
+
+    if (abrirReceta && panelReceta) {
+        abrirReceta.addEventListener("click", () => {
+            panelReceta.classList.add("open");
+            overlay.classList.add("open"); // Hacer visible el overlay
+        });
+
+        cerrarReceta.addEventListener("click", () => {
+            panelReceta.classList.remove("open");
+            overlay.classList.remove("open"); // Ocultar el overlay
+        });
+    }
+
+    // Cerrar modal si se hace clic en el overlay
+    if (overlay) {
+        overlay.addEventListener("click", () => {
+            panelReceta.classList.remove("open");
+            overlay.classList.remove("open");
+        });
+    }
 });
