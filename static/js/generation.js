91c85eec60b22621c0c961622c0f1a3e2c1fece8
silencecoder3000 <carlos.gallegos03@epn.edu.ec>
2024-12-15 12:39:00 -0500

    Targeteros

--- static/js/generation.js
+++ static/js/generation.js
@@ -1,7 +1,28 @@
 const recetas = [
-  { title: "Receta 1", description: "COCONUT" },
-  { title: "Receta 2", description: "ARROZ con Pescado" },
-  { title: "Receta 3", description: "Pollo Aguacate" },
+  {
+    title: "Receta 1",
+    description: "COCONUT",
+    proteinas: 5, // Gramos de proteína
+    grasas: 20, // Gramos de grasa
+    carbohidratos: 15, // Gramos de carbohidratos
+    calorias: 240, // Calorías totales
+  },
+  {
+    title: "Receta 2",
+    description: "ARROZ con Pescado",
+    proteinas: 25,
+    grasas: 10,
+    carbohidratos: 40,
+    calorias: 400,
+  },
+  {
+    title: "Receta 3",
+    description: "Pollo Aguacate",
+    proteinas: 30,
+    grasas: 18,
+    carbohidratos: 5,
+    calorias: 350,
+  },
 ];
 
 const postContainer = document.querySelector(".card-container");
@@ -9,13 +30,31 @@ const postContainer = document.querySelector(".card-container");
 const postMethods = () => {
   recetas.map((postData) => {
     // Crear un elemento HTML para cada receta
+
     const card = document.createElement("div");
     card.classList.add("card");
 
     // Agregar contenido a la tarjeta
     card.innerHTML = `
-        <h3>${postData.title}</h3>
-        <p>${postData.description}</p>
+        <div>
+          <h3>${postData.title}</h3>
+          <img src="${postData.image}" alt="${postData.title}" class=".image">
+        </div>      
+       <div>
+        <p><strong>Proteínas:</strong> ${postData.proteinas}g</p>
+        <div class="separator"></div>
+        <p><strong>Grasas:</strong> ${postData.grasas}g</p>
+        <div class="separator"></div>
+        <p><strong>Carbohidratos:</strong> ${postData.carbohidratos}g</p>
+        <div class="separator"></div>
+        <p><strong>Calorías:</strong> ${postData.calorias}kcal</p>
+      </div>
+        
+        <button class="btn-ready"  onclick="window.location.href='https://example.com/${postData.title
+          .replace(/ /g, "-")
+          .toLowerCase()}'">Agregar a plan</button>
+         
+
       `;
 
     // Añadir la tarjeta al contenedor
