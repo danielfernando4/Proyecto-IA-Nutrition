651aa1b223653e9a483ce6f6669063f4b00a87e8
silencecoder3000 <carlos.gallegos03@epn.edu.ec>
2024-12-15 13:52:04 -0500

    Targetas final sin animacion

--- static/js/generation.js
+++ static/js/generation.js
@@ -4,6 +4,7 @@ const recetas = [
     description: "COCONUT",
     proteinas: 5, // Gramos de proteína
     grasas: 20, // Gramos de grasa
+    image: "/static/images/comida112.jpg",
     carbohidratos: 15, // Gramos de carbohidratos
     calorias: 240, // Calorías totales
   },
@@ -11,6 +12,7 @@ const recetas = [
     title: "Receta 2",
     description: "ARROZ con Pescado",
     proteinas: 25,
+    image: "/static/images/comida112.jpg",
     grasas: 10,
     carbohidratos: 40,
     calorias: 400,
@@ -19,6 +21,7 @@ const recetas = [
     title: "Receta 3",
     description: "Pollo Aguacate",
     proteinas: 30,
+    image: "/static/images/comida112.jpg",
     grasas: 18,
     carbohidratos: 5,
     calorias: 350,
@@ -36,26 +39,29 @@ const postMethods = () => {
 
     // Agregar contenido a la tarjeta
     card.innerHTML = `
-        <div>
+      <div class = "recipe-cards">
+          <div>
           <h3>${postData.title}</h3>
           <img src="${postData.image}" alt="${postData.title}" class=".image">
-        </div>      
-       <div>
-        <p><strong>Proteínas:</strong> ${postData.proteinas}g</p>
-        <div class="separator"></div>
-        <p><strong>Grasas:</strong> ${postData.grasas}g</p>
-        <div class="separator"></div>
-        <p><strong>Carbohidratos:</strong> ${postData.carbohidratos}g</p>
-        <div class="separator"></div>
-        <p><strong>Calorías:</strong> ${postData.calorias}kcal</p>
-      </div>
-        
+          </div> 
+
+          <div>
+          <p><strong>Proteínas:</strong> ${postData.proteinas}g</p>
+          <div class="separator"></div>
+          <p><strong>Grasas:</strong> ${postData.grasas}g</p>
+          <div class="separator"></div>
+          <p><strong>Carbohidratos:</strong> ${postData.carbohidratos}g</p>
+          <div class="separator"></div>
+          <p><strong>Calorías:</strong> ${postData.calorias}kcal</p>
+         </div>
+       
+        <div >
         <button class="btn-ready"  onclick="window.location.href='https://example.com/${postData.title
           .replace(/ /g, "-")
           .toLowerCase()}'">Agregar a plan</button>
-         
-
-      `;
+         </div>
+      </div>
+  `;
 
     // Añadir la tarjeta al contenedor
     postContainer.appendChild(card);
