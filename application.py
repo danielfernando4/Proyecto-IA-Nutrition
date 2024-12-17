7e5ef5c17f7f74e79967319762e6c54f61ae4a05
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@epn.edu.ec>
2024-12-17 07:28:42 -0500

    endpoint para ti

--- application.py
+++ application.py
@@ -298,6 +298,15 @@ def kmeans():
 
 
 
+
+@app.rotue("/para_ti")
+def para_ti():
+    if "correo" in session and "id_usuario" in session:
+        return render_template("para_ti.html")
+    else:
+        return redirect(url_for("homepage"))
+
+
 @app.route("/datos")
 def datos():
     comidas = Comida.query.all()
